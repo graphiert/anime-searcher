28fb5b8575fe4b878ce0cfa597ab42488c751bdc
galihpujiirianto <galihpujiirianto@gmail.com>
2022-05-02 13:48:14 +0700

    CARE: cleaning up code

--- script.js
+++ script.js
@@ -1,61 +1,39 @@
 const searchBar = document.querySelector('#searchbar')
-const buttonSearch = document.querySelector('#button-search')
 const results = document.querySelector('#results')
 const spinner = document.querySelector('.spinner-border')
 const notFound = document.querySelector('.not-found')
 const container = document.querySelector('.container')
 
-const appendCards = results => {
-  if (results.year == null) {
-    $('#results').append(`
-      <div class="col-md-4 mt-3">
-        <div class="card">
-        <img src="${results.images.jpg.image_url}" class="card-img-top">
-        <div class="card-body">
-        <h5 class="card-title">${results.title}</h5>
-        <a href="#" data-mal-id="${results.mal_id}" class="card-link"
-           data-bs-toggle="modal" data-bs-target="#detailAnime">Click for more info!</a>
-      </div></div></div>`)
-    } else {
-    $('#results').append(`
-      <div class="col-md-4 mt-3">
-        <div class="card">
-        <img src="${results.images.jpg.image_url}" class="card-img-top">
-        <div class="card-body">
-        <h5 class="card-title">${results.title}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">${results.year}</h6>
-        <a href="#" data-mal-id="${results.mal_id}" class="card-link"
-           data-bs-toggle="modal" data-bs-target="#detailAnime">Click for more info!</a>
-      </div></div></div>`)
-    }
+const whileSearchAnime = () => {
+  results.innerHTML = ''
+  spinner.classList.add('d-none')
+  searchBar.value = ''
+}
+
+const infoCards = results => {
+  let img = results.images.jpg.image_url
+  let title = results.title
+  let year
+  results.year == null ? year = "Unknown" : year = results.year
+  let malId = results.mal_id
+  
+  return [img, title, year, malId]
 }
 
 const infoAnime = result => {
-  const modalBody = document.querySelector('.modal-body')
-  const malDetailed = document.querySelector('.mal-detailed')
-  modalBody.innerHTML ='...'
+  let img = result.images.jpg.large_image_url
+  let title = result.title
+  let score
+  result.score == null ? score = "Score: Not rated yet" : score = `Score: ${result.score}`
+  let typeEps = `${result.type}, ${result.episodes} ep(s)`
+  let duration = `Duration: ${result.duration}`
   let stud = []
+  let studio
   result.studios.forEach(el => stud.push(el.name))
-  let studio = stud.join(', ')
-  malDetailed.setAttribute('href', result.url)
-  $('.modal-body').html(`
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-4">
-        <img src="${result.images.jpg.large_image_url}" class ="img-fluid">
-      </div>
-      <div class="col-md-8">
-        <ul class="list-group">
-          <li class="list-group-item"><h4>${result.title}</h4></li>
-          <li class="list-group-item">Score: ${result.score}</li>
-          <li class="list-group-item">${result.type}, ${result.episodes} eps</li>
-          <li class="list-group-item">Duration: ${result.duration}</li>
-          <li class="list-group-item">Studios: ${studio}</li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  `)
+  stud.length == 0 ? studio = "Studio: Unknown" : studio = "Studio: " + stud.join(', ')
+  let malUrl = result.url
+  
+  return [img, title, score, typeEps, duration, studio, malUrl]
 }
 
 const searchAnime = () => {
@@ -65,27 +43,32 @@ const searchAnime = () => {
   fetch(`https://api.jikan.moe/v4/anime?q=${search}`)
   .then(responses => responses.json())
   .then(responses => {
-    let resultsApi = responses.data
-    if (resultsApi.length == 0) {
-      results.innerHTML = ''
+    if (responses.data.length == 0) {
+      whileSearchAnime()
       notFound.classList.remove('d-none')
-      spinner.classList.add('d-none')
-      searchBar.value = ''
     } else {
-      results.innerHTML = ''
-      spinner.classList.add('d-none')
-      resultsApi.forEach(res => appendCards(res))
-      searchBar.value = ''
+      whileSearchAnime()
+      responses.data.forEach(res => {
+        let data = infoCards(res)
+        $('#results').append(`
+          <div class="col-md-4 mt-3">
+            <div class="card">
+            <img src="${data[0]}" class="card-img-top">
+            <div class="card-body">
+            <h5 class="card-title">${data[1]}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${data[2]}</h6>
+            <a href="#" data-mal-id="${data[3]}" class="card-link"
+               data-bs-toggle="modal" data-bs-target="#detailAnime">Click for more info!</a>
+          </div></div></div>`)
+      })
     }
   })
   .catch(err => console.log(err))
 }
 
-buttonSearch.addEventListener('click', () => searchAnime())
-
-searchbar.addEventListener('keyup', e => {
+searchBar.addEventListener('keyup', e => {
   if (e.which === 13) {
-    searchAnime() 
+    searchAnime()
   }
 })
 
@@ -95,8 +78,26 @@ container.addEventListener('click', e => {
     fetch(`https://api.jikan.moe/v4/anime/${clickedId}`)
     .then(responses => responses.json())
     .then(responses => {
-      let resultsApi = responses.data
-      infoAnime(resultsApi)
+      let data = infoAnime(responses.data)
+      const modalBody = document.querySelector('.modal-body')
+      const malDetailed = document.querySelector('.mal-detailed')
+      modalBody.innerHTML ='...'
+
+      malDetailed.setAttribute('href', data[6])
+      $('.modal-body').html(`
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-4">
+                <img src="${data[0]}" class ="img-fluid">
+              </div>
+              <div class="col-md-8">
+                <ul class="list-group">
+                  <li class="list-group-item"><h4>${data[1]}</h4></li>
+                  <li class="list-group-item">${data[2]}</li>
+                  <li class="list-group-item">${data[3]}</li>
+                  <li class="list-group-item">${data[4]}</li>
+                  <li class="list-group-item">${data[5]}</li>
+                </ul></div></div></div>`)
      })
     .catch(err => console.log(err))
   }
